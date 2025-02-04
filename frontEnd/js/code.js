8b1cc19e36e54267fe6e2bda5fd0859ae00c371f
sw326 <sungwoo326@gmail.com>
2025-02-04 16:27:40 +0900

    aa

---
+++ frontEnd/js/code.js
@@ -0,0 +1,11 @@
+const txt = 'Hello, World!';
+console.log('Hello, World!');
+console.log(txt);
+console.log(`result: ${txt}`);
+
+function clr() {
+  console.clear();
+  console.log('Cleared');
+}
+
+//clr();
